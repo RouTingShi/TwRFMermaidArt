f751802b793a9fcba07ccb2ad427b64a55d2caa7
RouTingShi <bleach7306@gmail.com>
2023-01-19 17:49:17 +0800

    mobile's header completed

--- src/app/sharedModule/sharedMaterialModule.ts
+++ src/app/sharedModule/sharedMaterialModule.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   exports: [
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ] 
 })
 export class SharedMaterialModule {}
