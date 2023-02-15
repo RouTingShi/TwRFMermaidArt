edc8bc420890b88fc8d207abe0d4fa1fd409a542
RouTingShi <bleach7306@gmail.com>
2023-02-15 14:31:25 +0800

    解衝突

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-
-const routes: Routes = [
-  // { path: 'index', component: IndexComponent }
-=======
 import { ProductDirectoryComponent } from './views/product-directory/product-directory.component';
 
 const routes: Routes = [
   // { path: 'index', component: IndexComponent }
   { path: 'productDirctory', component: ProductDirectoryComponent }
->>>>>>> feat_createProductCardComponent
 ];
 
 @NgModule({
